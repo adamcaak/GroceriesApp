c0bc87901ab7d36a46842edaae517d5e9848014c
adamcaak <ladamczak1997@gmail.com>
2025-06-01 22:58:46 +0200

    Create SplashScrean
    Create loading view.

---
+++ GroceriesApp/Views/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  GroceriesApp
+//
+//  Created by Łukasz Adamczak on 01/06/2025.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
