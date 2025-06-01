6d3d3ad842e4ab365668900e9bac653e6edb3e15
adamcaak <ladamczak1997@gmail.com>
2025-06-01 22:22:16 +0200

    Initial Commit

---
+++ GroceriesApp/GroceriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesAppApp.swift
+//  GroceriesApp
+//
+//  Created by Łukasz Adamczak on 01/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
